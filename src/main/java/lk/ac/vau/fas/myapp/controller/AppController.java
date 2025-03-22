5f98f5a8829f57f392840b6629270f664baef2b5
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-03-22 15:42:44 +0530

    Message Endpoint: Returning a Greeting from Spring Boot

--- src/main/java/lk/ac/vau/fas/myapp/controller/AppController.java
+++ src/main/java/lk/ac/vau/fas/myapp/controller/AppController.java
@@ -2,9 +2,16 @@ package lk.ac.vau.fas.myapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/app")
 public class AppController {
+    @GetMapping("msg")
+    public String myMessage() {
+        return "Hello SpringBoot";
+    }
+    
     
 }
