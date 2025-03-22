1cf468c90431550e90c995f4a43f481f72b8ca1b
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-03-22 15:39:07 +0530

    Maps base paths to the controller

--- src/main/java/lk/ac/vau/fas/myapp/controller/AppController.java
+++ src/main/java/lk/ac/vau/fas/myapp/controller/AppController.java
@@ -1,8 +1,10 @@
 package lk.ac.vau.fas.myapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/app")
 public class AppController {
     
 }
