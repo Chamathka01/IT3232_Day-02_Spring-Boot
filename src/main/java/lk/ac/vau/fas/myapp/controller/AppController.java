4e2c082ec326a6f30504e3eb0ef43154e9762471
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-03-29 12:27:10 +0530

    Get Mapping with PathVariable for Dynamic Age Display

--- src/main/java/lk/ac/vau/fas/myapp/controller/AppController.java
+++ src/main/java/lk/ac/vau/fas/myapp/controller/AppController.java
@@ -3,6 +3,7 @@ package lk.ac.vau.fas.myapp.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -16,6 +17,9 @@ public class AppController {
 	public String myName() {
 		return "My name is SpringBoot";
 	}
-    
+    @GetMapping("/age/{ag}")
+	public String MyAge(@PathVariable("ag") int age) {
+		return "My age is "+age;
+	}
     
 }
