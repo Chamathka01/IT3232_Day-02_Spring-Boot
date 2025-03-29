f441a09ca6439970bc06d28b47d94bcc9ba4bdec
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-03-29 12:33:01 +0530

    Defining a class with some instance variables

--- src/main/java/lk/ac/vau/fas/myapp/model/Student.java
+++ src/main/java/lk/ac/vau/fas/myapp/model/Student.java
@@ -1,5 +1,11 @@
 package lk.ac.vau.fas.myapp.model;
 
 public class Student {
+    private String regNo;
+	private String name;
+	private int age;
+	private String course;
+	private double gpa;
+
     
 }
