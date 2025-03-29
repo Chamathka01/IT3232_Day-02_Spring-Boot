2160488b1900136d370547126ff658045da257cc
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-03-29 12:37:32 +0530

    Create some student objects

--- src/main/java/lk/ac/vau/fas/myapp/controller/AppController.java
+++ src/main/java/lk/ac/vau/fas/myapp/controller/AppController.java
@@ -2,6 +2,9 @@ package lk.ac.vau.fas.myapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lk.ac.vau.fas.myapp.model.Student;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,6 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 @RequestMapping("/app")
 public class AppController {
+
+    Student Bob = new Student("2020IT01","Bob Marely",23,"IT",3.2);
+	Student Amal = new Student("2020IT02","Amal Perera",24,"AMC",3.7);
+	Student Nimal = new Student("2020IT03","Nimal Hettiarachchi",22,"IT",3.0);
+
     @GetMapping("msg")
     public String myMessage() {
         return "Hello SpringBoot";
